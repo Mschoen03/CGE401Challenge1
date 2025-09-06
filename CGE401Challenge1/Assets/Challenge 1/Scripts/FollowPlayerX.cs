4ff3e2ee085ac3aa5db0a6722f52c946cdaffa04
Mschoen03 <mschoen03a@gmail.com>
2025-09-06 13:57:03 -0500

    Fin project

---
+++ CGE401Challenge1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Matt Schoen
+ * Challenge1
+ * Updated Cam-Follow-Player
+ */
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ CGE401Challenge1/Assets/Challenge 1/Scripts/PropellerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Matt Schoen
+ * Challenge1
+ * Propller rotates
+ */
+
+public class PropellerX : MonoBehaviour
+{
+    public float rotationSpeed = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+
+    }
+}
